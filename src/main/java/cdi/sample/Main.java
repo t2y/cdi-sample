2c78034c037c29964aa020aa6cb115c1d3b33d1a
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2020-01-15 01:03:09 +0900

    add to compare hash value of instances which the container selects

--- src/main/java/cdi/sample/Main.java
+++ src/main/java/cdi/sample/Main.java
@@ -6,7 +6,6 @@ import org.jboss.weld.environment.se.Weld;
 
 import cdi.sample.dependencyinjection.MyApp;
 import cdi.sample.dependencyinjection.MyClient;
-import cdi.sample.dependencyinjection.MyProducer;
 import cdi.sample.observer.SimpleEvent;
 import lombok.val;
 import lombok.extern.slf4j.Slf4j;
@@ -18,15 +17,20 @@ public class Main {
     val weld = new Weld().enableDiscovery();
     val container = weld.initialize();
 
-    //val p = new MyProducer(container);
-
-    val myClient = container.select(MyClient.class).get();
-    log.info("id: " + String.valueOf(myClient.getId()));
-    log.info("name: " + myClient.getName());
+    val myClient1 = container.select(MyClient.class).get();
+    log.info("id: " + myClient1.getId());
+    log.info("name: " + myClient1.getName());
+    log.info("hash: " + myClient1.hashCode());
     log.info("------------------------------------------------------------------------");
 
     val myApp = container.select(MyApp.class).get();
     log.info("id: " + String.valueOf(myApp.getMyClient().getId()));
+    log.info("------------------------------------------------------------------------");
+
+    val myClient2 = container.select(MyClient.class).get();
+    log.info("id: " + myClient2.getId());
+    log.info("name: " + myClient2.getName());
+    log.info("hash: " + myClient2.hashCode());
     log.info("------------------------------------------------------------------------");
   }
 
